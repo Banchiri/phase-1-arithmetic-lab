535fd6c5f65df0edad5d864d0a791c788d548143
sandrabanchiri <sandrabanchiri39@gmail.com>
2024-07-04 15:28:18 +0300

    arithmetic file

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
-
-// Write your code here
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
+let random = Math.ceil(Math.random() * 10 ) + 1;
+let num3 =10;
+let num4 = 6;
+let mod = num3 % num4;
+let max = Math.max(4, 18, 13, 14, 19, 7, 20);
+console.log(max);
